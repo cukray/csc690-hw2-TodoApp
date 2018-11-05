658f4f12885231a4d9527d3570748b4fe5972a7c
CUK <CUK@CUKs-MacBook-Pro.local>
2018-11-05 00:06:27 -0800

    commit

---
+++ TodoAPp/TodoAPp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TodoApp
+//
+//  Created by CUK on 11/1/18.
+//  Copyright © 2018 U Keong Cheong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
